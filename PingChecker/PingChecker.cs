9e74b1496ad2fc93a513bee2769d208d6bcdcc5a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2016-06-03 02:51:30 +0200

    Initial code

---
+++ PingChecker/PingChecker.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace MyPing
+{
+    public enum PingStatus
+    {
+        Ok,
+        Error
+    };
+
+    public class PingResult
+    {
+        internal PingResult(long ping)
+        {
+            Ping = ping;
+            Status = PingStatus.Ok;
+        }
+
+        internal PingResult(PingStatus status)
+        {
+            Ping = null;
+            Status = status;
+        }
+
+        public long? Ping { get; internal set; }
+        public PingStatus Status { get; internal set; }
+    }
+
+
+    public abstract class PingChecker
+    {
+        public abstract PingResult Get();
+
+        public String HostString { get; protected set; }
+
+        public virtual bool SetHost(String host)
+        {
+            HostString = host;
+
+            if (HostChanged != null)
+                HostChanged(host);
+
+            return true;
+        }
+        public event Action<String> HostChanged;
+    }
+
+    class RandomPingChecker : PingChecker
+    {
+        private readonly Random _rnd = new Random();
+
+        public override PingResult Get()
+        {
+            var p =_rnd.Next(400);
+            return (p > 300) ? new PingResult(PingStatus.Error) : new PingResult(p);
+        }
+    }
+
+    class RealPingChecker : PingChecker
+    {
+        protected IPAddress HostIpAddress;
+        private readonly Ping  _pingSender = new Ping();
+
+        public RealPingChecker(String host = "8.8.8.8")
+        {
+            SetHost(host);
+        }
+
+        public override bool SetHost(String host)
+        {
+            try
+            {
+                HostIpAddress = IPAddress.Parse(host);
+                return base.SetHost(host);
+            }
+            catch (Exception)
+            {
+                HostIpAddress = null;
+                base.SetHost("");
+            }
+            return false;
+        }
+
+        public override PingResult Get()
+        {
+            if (HostIpAddress != null)
+            {
+                try
+                {
+                    PingReply reply = _pingSender.Send(HostIpAddress);
+
+                    if (reply != null && reply.Status == IPStatus.Success)
+                        return new PingResult(reply.RoundtripTime);
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            return new PingResult(PingStatus.Error);
+        }
+    }
+}
---
+++ PingChecker/PingCheckerContext.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.Threading;
+using System.Windows.Forms;
+using MyPing;
+using PingChecker.Properties;
+
+
+namespace PingChecker
+{
+    class PingCheckerContext : ApplicationContext
+    {
+        private NotifyIcon _trayIcon;
+        private Icon _noConnectionIcon = null;
+        private Font _font = new Font(new FontFamily("Lucida Console"), 8);
+        private MyPing.PingChecker PingChecker = null;
+
+        public PingCheckerContext()
+        {
+            StartPinging();
+            SetupContextMenu();
+        }
+
+        private Icon NoConnectionIcon
+        {
+            get
+            {
+                if (_noConnectionIcon == null)
+                {
+                    var bmp = new Bitmap(16, 16, PixelFormat.Format32bppArgb);
+                    var bc = Color.DarkRed;
+
+                    using (var g = Graphics.FromImage(bmp))
+                    {
+                        g.FillRectangle(new SolidBrush(bc), 1, 1, 14, 14);
+                    }
+                    _noConnectionIcon = FlimFlan.IconEncoder.Converter.BitmapToIcon(bmp);
+                }
+
+                return _noConnectionIcon;
+            }
+        }
+
+        private Icon PingIcon(long ping)
+        {
+            // 30 ping - 120 H (green)
+            // 200 ping - 0 H (red)
+
+            // using linear function hue = a * ping + b to dermine hue
+            // we get a ~= 0.7 and b ~= 141
+
+            var hue = -0.7*ping + 141; 
+
+            // clamp value to [0, 120] range
+
+            hue = Math.Min(Math.Max(0, hue), 120);
+
+            // get Color from calculated hue
+            var color = HSBtoColor.HsvToRgb(hue, 1, 1);
+
+            var bmp = new Bitmap(16, 16, PixelFormat.Format32bppArgb);
+            using (var g = Graphics.FromImage(bmp))
+            {
+                g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+                var sf = new StringFormat()
+                {
+                    Alignment = StringAlignment.Center
+                };
+
+                g.DrawString(ping.ToString(),
+                    _font,
+                    new SolidBrush(color),
+                    8, 4, sf);
+            }
+
+            return FlimFlan.IconEncoder.Converter.BitmapToIcon(bmp);
+        }
+
+        private void SetupContextMenu()
+        {
+            // current host item
+            var currentHostItem = new ToolStripLabel(String.Format("IP: {0}", PingChecker.HostString))
+            {
+                ForeColor = Color.Gray
+            };
+            if (PingChecker != null)
+            {
+                PingChecker.HostChanged += delegate(String host)
+                {
+                    currentHostItem.Text = String.Format("IP: {0}", host);
+                    if (host != Settings.Default.PingedHost)
+                    {
+                        Settings.Default.PingedHost = host;
+                        Settings.Default.Save();
+                    }
+                };
+            }
+
+            // change host item
+            var changeHostItem = new ToolStripMenuItem(Resources.ChangeHostText);
+            changeHostItem.Click += delegate
+            {
+                if (PingChecker == null) return;
+
+                var newHost = Microsoft.VisualBasic.Interaction.InputBox("Input new IP Adress", "Change IP", PingChecker.HostString);
+                if (newHost.Length > 0)
+                {
+                    PingChecker.SetHost(newHost);
+                }
+            };
+
+            // close item
+            var contextMenuCloseItem = new ToolStripMenuItem(Resources.CloseText);
+
+            // context menu
+            var contextMenuStrip = new ContextMenuStrip();
+            contextMenuStrip.Items.Add(currentHostItem);
+            contextMenuStrip.Items.Add(changeHostItem);
+            contextMenuStrip.Items.Add(new ToolStripSeparator());
+            contextMenuStrip.Items.Add(contextMenuCloseItem);
+            
+            // icon
+            _trayIcon = new NotifyIcon()
+            {
+                ContextMenuStrip = contextMenuStrip,
+                Text = Resources.PingCheckerTitle,
+                Visible =  true,
+                Icon = NoConnectionIcon
+
+            };
+
+            contextMenuCloseItem.Click += delegate
+            {
+                _trayIcon.Visible = false;
+                Application.Exit();
+            };
+        }
+
+        private void ping_NewValue(object sender, ProgressChangedEventArgs e)
+        {
+            if (_trayIcon == null) return;
+
+            var result = e.UserState as PingResult;
+            if (result == null) return;
+
+            _trayIcon.Icon = (result.Status == PingStatus.Ok && result.Ping.HasValue) ? PingIcon(result.Ping.Value) : NoConnectionIcon;
+        }
+
+        private void ping_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var bw = sender as BackgroundWorker;
+            if (bw == null) return;
+
+            bw.ReportProgress(0, PingChecker.Get());
+
+            while (!bw.CancellationPending)
+            {
+                Thread.Sleep(1000);
+                bw.ReportProgress(0, PingChecker.Get());
+            }
+        }
+
+        private void StartPinging()
+        {
+            PingChecker = new RealPingChecker(Settings.Default.PingedHost);
+            var backgroundWorker = new BackgroundWorker()
+            {
+                WorkerReportsProgress = true
+            };
+
+            backgroundWorker.ProgressChanged += ping_NewValue;
+            backgroundWorker.DoWork += ping_DoWork;
+            backgroundWorker.RunWorkerAsync();
+        }
+    }
+}
